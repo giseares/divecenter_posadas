f258b4d6a5feecbbf2ffe114ccd8bc94bd230500
Gise Ares <gise.ares@gmail.com>
2020-08-29 15:04:14 -0300

    routes done: eError: uninitialized constant AgenciesController

--- app/controllers/agencies.rb
+++ app/controllers/agencies.rb
@@ -1,5 +1,4 @@
 class AgenciesController < ApplicationController
-  skip_before_action :authenticate_user!
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'agencies#home'
+  root to: 'agencies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :agencies, only: [:index, :show]
 end
